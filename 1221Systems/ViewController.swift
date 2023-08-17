bb2b98f0b763a85c67527ca87b2c95c2e0b7fe5c
Nurgali Nuraly <nurali.nurgali@gmail.com>
2023-08-17 20:13:41 +0600

    Initial Commit

---
+++ 1221Systems/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1221Systems
+//
+//  Created by Nurgali on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
